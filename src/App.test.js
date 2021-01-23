d2378137ed45b24bec118f8899796df5b7968dd8
jeromeski <jeromegee@gmail.com>
2021-01-23 20:00:56 +0800

    Our first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = document.querySelector('div');
+  expect(div.innerHTML).toContain('Hi there!');
 });
